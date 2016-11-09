543db65cfc7a04ea66fdd41b720c5a64c8366276
Abhinaw Tripathi <abhinawtripathi34@gmail.com>
2016-11-09 19:34:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,8 @@ def makeWebhookResult(data):
         "type": "template",
         "payload": {"template_type" : "button", "text" : "what do you want?"}
        }
-      },
+        }
+        },
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
